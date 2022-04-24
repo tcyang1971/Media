78114699f2746ee98d72011aa1d399113480ff4a
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-24 11:00:45 +0800

    v.3播放聲音檔

--- app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +22,19 @@ class MainActivity : AppCompatActivity() {
 
         //設定螢幕水平顯示
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+    }
+
+    fun StartPlay(v: View){
+        when (v.id) {
+            R.id.btn1 -> {
+                mper = MediaPlayer.create(this, R.raw.tcyang)
+                mper.start()
+            }
+
+            R.id.btn2 -> {
+                mper = MediaPlayer.create(this, R.raw.fly)
+                mper.start()
+            }
+        }
     }
 }
