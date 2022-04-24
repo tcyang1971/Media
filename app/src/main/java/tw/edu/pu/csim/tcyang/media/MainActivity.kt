0cdbb08fc297f21c284a02b277326ee30708c6fb
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-24 10:56:52 +0800

    v.2橫向全螢幕

--- app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
@@ -1,11 +1,25 @@
 package tw.edu.pu.csim.tcyang.media
 
+import android.content.pm.ActivityInfo
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mper: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mper = MediaPlayer()
+
+        //不要自動休眠
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        //設定螢幕水平顯示
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
     }
 }
